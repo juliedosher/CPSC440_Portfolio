c3457e5642c79ae272d4262a776c2833deb215aa
Julie Dosher <112606564+juliedosher@users.noreply.github.com>
2023-02-12 13:33:54 -0600

    Update and rename server.js to index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let handleRequest = (request, response) => {
     response.writeHead(200, {
         'Content-Type': 'text/html'
     });
-    fs.readFile('./index.html', null, function (error, data) {
+    fs.readFile('./site.html', null, function (error, data) {
         if (error) {
             response.writeHead(404);
             respone.write('Whoops! File not found!');
